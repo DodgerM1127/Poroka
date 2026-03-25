6b56d3afe7d3c1d020593970d95508dd765fe79a
Matej1127 <53857750+DodgerM1127@users.noreply.github.com>
2026-03-25 09:21:26 +0100

    countdown width

--- components/Countdown.js
+++ components/Countdown.js
@@ -24,22 +24,22 @@ export default function Countdown({ targetDate }) {
   if (timeLeft == null) return <div className="text-xl">The wedding day has arrived 🎉</div>
 
   return (
-    <div className="inline-flex flex-wrap justify-center gap-6 text-center mx-auto">
-      <div className="w-24">
-        <div className="text-4xl font-semibold">{timeLeft.days}</div>
-        <div className="text-sm text-gray-500">dni</div>
+    <div className="inline-flex flex-wrap justify-center gap-3 sm:gap-6 text-center mx-auto max-w-[350px]">
+      <div className="w-14 sm:w-20 md:w-24">
+        <div className="text-3xl sm:text-4xl font-semibold">{timeLeft.days}</div>
+        <div className="text-xs sm:text-sm text-gray-500">dni</div>
       </div>
-      <div className="w-24">
-        <div className="text-4xl font-semibold">{timeLeft.hours}</div>
-        <div className="text-sm text-gray-500">ure</div>
+      <div className="w-14 sm:w-20 md:w-24">
+        <div className="text-3xl sm:text-4xl font-semibold">{timeLeft.hours}</div>
+        <div className="text-xs sm:text-sm text-gray-500">ure</div>
       </div>
-      <div className="w-24">
-        <div className="text-4xl font-semibold">{timeLeft.minutes}</div>
-        <div className="text-sm text-gray-500">minute</div>
+      <div className="w-14 sm:w-20 md:w-24">
+        <div className="text-3xl sm:text-4xl font-semibold">{timeLeft.minutes}</div>
+        <div className="text-xs sm:text-sm text-gray-500">minute</div>
       </div>
-      <div className="w-24">
-        <div className="text-4xl font-semibold">{timeLeft.seconds}</div>
-        <div className="text-sm text-gray-500">sekunde</div>
+      <div className="w-14 sm:w-20 md:w-24">
+        <div className="text-3xl sm:text-4xl font-semibold">{timeLeft.seconds}</div>
+        <div className="text-xs sm:text-sm text-gray-500">sekunde</div>
       </div>
     </div>
   )
