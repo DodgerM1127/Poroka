2a6f72b45b57b096c6b4040de980c66bd7c722fa
Matej1127 <53857750+DodgerM1127@users.noreply.github.com>
2026-08-11 20:58:26 +0200

    besedilo nad videom

--- pages/povabljeni.js
+++ pages/povabljeni.js
@@ -134,7 +134,8 @@ export default function Povabljeni() {
       <section id="help" className="max-w-6xl mx-auto px-6 py-16">
         <h3 className="text-3xl font-bold text-[#b17253] mb-6 text-center">Ali želiš pomagati?</h3>
         <p className="text-gray-600 text-lg leading-relaxed text-center">Ker sva že večkrat slišala, naj ne bova trmasta in ne delava vsega sama iščeva, kakšnega pekovskega mojstra ali vajenca, ki bi bil pripravljen speči kakšno dobroto. Če si želiš pomagati, se vpiši v <a href="https://docs.google.com/spreadsheets/d/1s5tt8S0iLu9WvMv3C8WelAbdJPP3aeCN2pZeOF8QZmQ/edit?usp=sharing" target="_blank" className="text-[#b17253] hover:underline">tabelo</a>.</p>
-        <div className="mt-6 flex justify-center">
+        <p className="text-lg md:text-xl text-gray-700 text-center mt-6">Video prikazuje kje bo zakuska in kam prinesti spečene dobrote.</p>
+        <div className="mt-4 flex justify-center">
           <video
             controls
             playsInline
